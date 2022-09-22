7f8bb09183ce48c08ad7637d3a2af31449608203
maxdupin <maximilien.dupin@etu.u-bordeaux.fr>
2022-09-22 12:15:48 +0200

    lucas oublie les acolades

--- code/userprog/progtest.cc
+++ code/userprog/progtest.cc
@@ -126,10 +126,11 @@ ConsoleDriverTest (const char *in, const char *out)
     char ch;
     ConsoleDriver *test_consoledriver = new ConsoleDriver(in, out);
     
-    while ((ch = test_consoledriver->GetChar()) != EOF)
+    while ((ch = test_consoledriver->GetChar()) != EOF){
         test_consoledriver->PutChar('<');
         test_consoledriver->PutChar(ch);
         test_consoledriver->PutChar('>');
+    }
     fprintf(stderr, "EOF detected in ConsoleDriver!\n");
     
     delete test_consoledriver;
