4a883309030cc9980d9fd3cc48920b478af0afcb
maxdupin <maximilien.dupin@etu.u-bordeaux.fr>
2022-10-04 15:25:13 +0200

    zfz

--- code/test/putchar.c
+++ code/test/putchar.c
@@ -11,11 +11,6 @@ void print(char c, int n)
 int
 main()
 {
-<<<<<<< HEAD
-    char tab[4]={'a','b','d','\0'};
-=======
-    char s [5] = {'a', 'b', 'c', 'd', '\0'};
->>>>>>> a956ed1c5fde82d63200df2ed848c8472dffd65f
     print('a',4);
     Halt();
 }
