ac2a4e4da1291baed61d76bc0d8bc6b2c3b3e742
maxdupin <maximilien.dupin@etu.u-bordeaux.fr>
2022-11-20 16:55:16 +0100

    Ajouts de l'ancienne fonction

--- code/test/makethreads.c
+++ code/test/makethreads.c
@@ -3,6 +3,7 @@
 Pour lancer le programme de test veuillez utiliser la commande suivante: 
 ./nachos -rs 1234 -x ../test/makethreads
 */
+
 volatile int i;
 void print(char c)
 {  
@@ -14,6 +15,7 @@ void print(char c)
 int
 main()
 {
+
     for(i = 0; i<10; i++){
         ThreadCreate(print, 'a');
     }
@@ -21,3 +23,23 @@ main()
     return 0;
 }
 
+//La partie en commentaire si dessous représente le premier test que l'on a implémenté avec les boucles 
+//while qui permettait en premier lieu au thread d'écrire le a comme voulu.
+/*
+void print(char c)
+{  
+    PutChar(c);
+    while(1);
+    PutChar('\n');
+}
+
+int
+main()
+{
+    ThreadCreate(print,'a');
+    while(1);
+    //ThreadExit(); 
+    return 0;
+}
+
+*/
